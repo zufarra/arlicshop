bfe4ea9d5e4a568759319f0c31da9792bb604a64
zufarra <zufarromli@gmail.com>
2024-09-10 21:55:48 +0700

    berhasil membuat beberapa perubahan pada html

--- main/views.py
+++ main/views.py
@@ -3,9 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name' : 'playstation',
-        'price': '60000',
-        'description': '4 hours'
+        'name' : 'Zufar Romli Amri',
+        'class': 'PBP D',
     }
 
     return render(request, "main.html", context)
